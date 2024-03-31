cf31715f881818b3b71311c45ff389f5afc06e28
djabirpc <djabir.pc@outlook.fr>
2024-03-31 15:28:04 +0200

    Loading Attendee Relations

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,20 +4,30 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    use CanLoadRelationships;
     /**
      * Display a listing of the resource.
      */
+    private array $relations = ['user','attendees','attendees.user'];
     public function index()
     {
+        
+        $query = $this->loadRelationships(Event::query());
+        
+        
+
         return EventResource::collection(
-            Event::with('user')->paginate()
+            $query->latest()->paginate()
         );
     }
+
+    
 
     /**
      * Store a newly created resource in storage.
@@ -34,7 +44,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
         
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
@@ -43,7 +53,7 @@ class EventController extends Controller
     public function show(Event $event)
     {
         $event->load('user','attendees');
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
@@ -60,7 +70,7 @@ class EventController extends Controller
             ])
         );
         
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
 
